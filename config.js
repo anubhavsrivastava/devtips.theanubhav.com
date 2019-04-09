41f5eeb18af90ef78a536dc6981c230097cb4af6
Anubhav Srivastava <anubhav.srivastava00@gmail.com>
2019-04-09 22:50:55 +0530

    removed media files

--- config.js
+++ config.js
@@ -1,38 +1,38 @@
 'use strict';
 
 module.exports = {
-  url: 'http://devtips.theanubhav.com',
-  title: 'Blog by John Doe',
-  subtitle: 'Pellentesque odio nisi, euismod in, pharetra a, ultricies in, diam. Sed arcu.',
-  copyright: '© All rights reserved.',
-  disqusShortname: '',
-  postsPerPage: 4,
-  googleAnalyticsId: '',
-  menu: [
-    {
-      label: 'Articles',
-      path: '/'
-    },
-    {
-      label: 'About me',
-      path: '/pages/about'
-    },
-    {
-      label: 'Contact me',
-      path: '/pages/contacts'
-    }
-  ],
-  author: {
-    name: 'John Doe',
-    photo: '/photo.jpg',
-    bio: 'Pellentesque odio nisi, euismod in, pharetra a, ultricies in, diam. Sed arcu.',
-    contacts: {
-      email: '#',
-      telegram: '#',
-      twitter: '#',
-      github: '#',
-      rss: '#',
-      vkontakte: '#'
-    }
-  }
+	url: 'http://devtips.theanubhav.com',
+	title: 'Quick tips by Anubhav Srivastava',
+	subtitle: 'Personal quick tips for Web Developers',
+	copyright: '.',
+	disqusShortname: 'theanubhav',
+	postsPerPage: 4,
+	googleAnalyticsId: '',
+	menu: [
+		{
+			label: 'Articles',
+			path: '/'
+		},
+		{
+			label: 'About me',
+			path: '/pages/about'
+		},
+		{
+			label: 'Contact me',
+			path: '/pages/contacts'
+		}
+	],
+	author: {
+		name: 'Anubhav Srivastava',
+		photo: 'https://avatars2.githubusercontent.com/u/6449301?s=460&v=4',
+		bio: 'Pellentesque odio nisi, euismod in, pharetra a, ultricies in, diam. Sed arcu.',
+		contacts: {
+			email: '#',
+			telegram: '#',
+			twitter: '#',
+			github: '#',
+			rss: '#',
+			vkontakte: '#'
+		}
+	}
 };
