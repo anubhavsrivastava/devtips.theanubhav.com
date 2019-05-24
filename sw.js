d7c2f7a46255b8619d5834d6aae847d65d1e2ee6
Anubhav Srivastava <anubhav.srivastava00@gmail.com>
2019-05-24 18:02:06 +0530

    Updates

--- sw.js
+++ sw.js
@@ -26,7 +26,7 @@ workbox.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-4ef316d0c61f0adceb73.js"
+    "url": "webpack-runtime-e774e2d92e7330f2cbd1.js"
   },
   {
     "url": "styles.6018ab4296815cfc2e0c.css"
@@ -42,7 +42,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "7dc91e83ef87fc5da3067238e4cdea6a"
+    "revision": "473964df921e03a456d508c3975e8615"
   },
   {
     "url": "static/d/604/path---offline-plugin-app-shell-fallback-a-30-c5a-BawJvyh36KKFwbrWPg4a4aYuc8.json"
