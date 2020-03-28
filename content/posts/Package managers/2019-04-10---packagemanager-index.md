db2a4c05b0c11ecc0b85dccbd81668a607687ebf
anubhavsrivastava <anubhav.srivastava00@gmail.com>
2020-03-28 17:56:02 +0100

    re-organised package manager

---
+++ content/posts/Package managers/2019-04-10---packagemanager-index.md
@@ -0,0 +1,13 @@
+---
+title: All About package managers and how to tame them!
+date: '2019-04-10T18:05:00.738Z'
+template: 'post'
+draft: false
+slug: '/posts/package-manager'
+category: ''
+description: 'Series of useful tips and infos about package manager, the way it is meant to be!'
+---
+
+This is a series of tips for using package manager in an efficient and productive manner. This series mostly focuses on `npm` and `yarn` (JavaScript) but would also focus on any other managers that are around other ecosystems. As we know, JavaScript package managers and module system has gone beyond control and its really a subject now, to learn them as well.
+
+You can check all articles under [`PackageManager`](/category/package-manager/) tag.
--- src/components/Sidebar/Author/Author.js
+++ src/components/Sidebar/Author/Author.js
@@ -1,76 +1,90 @@
-import React from 'react';
-import { withPrefix, Link } from 'gatsby';
-import styles from './Author.module.scss';
+import React from "react";
+import { Link } from "gatsby";
+import styles from "./Author.module.scss";
 
 const Author = ({ author, isIndex }) => (
-	<div className={styles['author']}>
-		<Link to="/">
-			<img src={author.photo} className={styles['author__photo']} width="75" height="75" alt={author.name} />
-		</Link>
+  <div className={styles["author"]}>
+    <Link to="/">
+      <img
+        src={author.photo}
+        className={styles["author__photo"]}
+        width="75"
+        height="75"
+        alt={author.name}
+      />
+    </Link>
 
-		{isIndex ? (
-			<h1 className={styles['author__title']}>
-				<Link className={styles['author__title-link']} to="/">
-					{author.name}
-				</Link>
-			</h1>
-		) : (
-			<h2 className={styles['author__title']}>
-				<Link className={styles['author__title-link']} to="/">
-					{author.name}
-				</Link>
-			</h2>
-		)}
-		<p className={styles['author__subtitle']}>{author.bio}</p>
-		<p className={styles['author__subtitle']}>
-			<a style={{ margin: '20px' }} href="//about.theanubhav.com">
-				About me
-			</a>
+    {isIndex ? (
+      <h1 className={styles["author__title"]}>
+        <Link className={styles["author__title-link"]} to="/">
+          {author.name}
+        </Link>
+      </h1>
+    ) : (
+      <h2 className={styles["author__title"]}>
+        <Link className={styles["author__title-link"]} to="/">
+          {author.name}
+        </Link>
+      </h2>
+    )}
 
-			<a style={{ margin: '20px' }} href="//theanubhav.com">
-				Blog
-			</a>
-		</p>
-		<p className={styles['author__subtitle']}>Personal quick tips for Web Developers</p>
+    <p className={styles["author__subtitle"]}>Personal quick tips for Web Developers</p>
 
-		<h2 className={styles['author__title']}>
-			<Link className={styles['author__title-link']} to="/">
-				Series Posts
-			</Link>
-		</h2>
-		<p className={styles['author__subtitle']}>
-			<Link style={{ margin: '20px' }} to="/category/cli-build-tool">
-				CLI Build Tool
-			</Link>
-			<br />
-			<Link style={{ margin: '20px' }} to="/category/vs-code-ide">
-				VSCode Extensions
-			</Link>
-			<br />
-			<Link style={{ margin: '20px' }} to="/category/js-pitfall">
-				JavaScript Pitfalls
-			</Link>
-		</p>
+    {/* <p className={styles["author__subtitle"]}>{author.bio}</p> */}
+    <p className={styles["author__subtitle"]}>
+      <a style={{ margin: "20px" }} href="//about.theanubhav.com">
+        About me
+      </a>
 
-		<h2 className={styles['author__title']}>
-			<Link className={styles['author__title-link']} to="/">
-				Links
-			</Link>
-		</h2>
-		<p className={styles['author__subtitle']}>
-			<Link style={{ margin: '20px' }} to="/all">
-				All Tips
-			</Link>
-			<br />
-			<Link style={{ margin: '20px' }} to="/categories">
-				Categories
-			</Link>
-			<br />
-			<Link style={{ margin: '20px' }} to="/tags">
-				Tags
-			</Link>
-		</p>
-	</div>
+      <a style={{ margin: "20px" }} href="//theanubhav.com">
+        Blog
+      </a>
+    </p>
+
+    <h2 className={styles["author__title"]}>
+      <Link className={styles["author__title-link"]} to="/">
+        Series Posts
+      </Link>
+    </h2>
+    <p className={styles["author__subtitle"]}>
+      <Link style={{ margin: "20px" }} to="/category/vs-code-ide">
+        VSCode Extensions
+      </Link>
+      <br />
+      <Link style={{ margin: "20px" }} to="/category/js-pitfall">
+        JavaScript Pitfalls
+      </Link>
+      <br />
+
+      <Link style={{ margin: "20px" }} to="/category/package-manager">
+        Package Managers
+      </Link>
+      <br />
+
+      <Link style={{ margin: "20px" }} to="/category/cli-build-tool">
+        CLI Build Tool
+      </Link>
+    </p>
+
+    <h2 className={styles["author__title"]}>
+      <Link className={styles["author__title-link"]} to="/">
+        Links
+      </Link>
+    </h2>
+    <p className={styles["author__subtitle"]}>
+      <Link style={{ margin: "20px" }} to="/all">
+        All Tips
+      </Link>
+      <br />
+      <Link style={{ margin: "20px" }} to="/categories">
+        Categories
+      </Link>
+      <br />
+      <Link style={{ margin: "20px" }} to="/tags">
+        Tags
+      </Link>
+    </p>
+  </div>
 );
 
 export default Author;
