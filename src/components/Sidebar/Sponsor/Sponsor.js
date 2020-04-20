a8c2d7475d6a22db3f3d6b53580b77fa4a21735e
anubhavsrivastava <anubhav.srivastava00@gmail.com>
2020-04-20 20:12:55 +0200

    added sponsor component

---
+++ src/components/Sidebar/Sponsor/Sponsor.js
@@ -0,0 +1,14 @@
+import React from "react";
+import styles from "./Sponsor.module.scss";
+
+const Contacts = () => (
+  <div className={styles["sponsor"]}>
+    Consider
+    <a target="_blank" href="https://theanubhav.com/sponsor">
+      {" "}
+      sponsoring
+    </a>
+  </div>
+);
+
+export default Contacts;
